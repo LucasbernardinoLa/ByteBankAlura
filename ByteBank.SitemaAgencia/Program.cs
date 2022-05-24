6b6cb355aa7a934bad78baa11dcc480cf3a42409
Lucas Bernardino <lucasblameu@gmail.com>
2022-05-24 10:57:44 -0300

    conteuda da aula 6 finalizado

--- ByteBank.SitemaAgencia/Program.cs
+++ ByteBank.SitemaAgencia/Program.cs
@@ -1,13 +1,8 @@
 ﻿using ByteBank.Modelos;
-using Humanizer;
+using System.Text.RegularExpressions;
 
+string padrao = "[0-9]{4,5}-?[0-9]{4}";
+string texto = "Meu número é: 2342-3453";
 
-// Dia 17 de Agosto de 2018
-DateTime dataFimPagamento = new DateTime(2022, 5, 22);
-// Data corrente no momento de execução do código
-DateTime dataCorrente = DateTime.Now;
-TimeSpan diferenca = dataFimPagamento - dataCorrente;
-
-string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
-
-Console.WriteLine(mensagem);
+Match match = Regex.Match(texto, padrao);
+Console.WriteLine(match.Value);
